44188a4b21d87523a9472afa9092297aee228867
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-12 19:20:46 -0700

    Adaptability Changes

---
+++ test.py
@@ -0,0 +1,3 @@
+import main
+arr = ["5",4,"3","2",1,15]
+print(main.summation(arr, mode="mixed"))
--- tread.py
+++ tread.py
@@ -1,13 +1,13 @@
-def summation(arr, type="Mixed"):
-    if(type == "Mixed"):
+def summation(arr, mode="mixed"):
+    if(mode == "mixed"):
         sum = 0
         for j in arr:
-            if(type(j) == int):
+            if(isinstance(j, int)):
                 sum += j
             elif(j.isnumeric()):
                 sum += int(j)
         return sum
-    elif(type == "int"):
+    elif(mode == "int"):
         sum = 0
         for j in arr:
             sum += j
@@ -17,7 +17,7 @@ def summation(arr, type="Mixed"):
         for j in arr:
             sum += int(j)
         return sum
-def average(arr, type="Mixed"):
+def average(arr, mode="Mixed"):
     sum = summation(arr)
     length = len(arr)
     return sum/length
@@ -27,9 +27,8 @@ def print_tab(arr):
 def print_space(arr):
     out = [str(i) for i in arr]
     print(" ".join(out))
-def or(a, b, ind=min(len(a),len(b))):
-    arr = []
-    for j in range(ind):
-        arr.append(a[j]|b[j])
-    return arr
-def skew(a, axis=0, bias=True):
+def make_array(string, d=""):
+    if(d == ""):
+        return list(string)
+    else:
+        return string.split(d)
