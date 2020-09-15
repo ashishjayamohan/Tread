d462f09f0b306e3818d8972ea93ce02f913a3acc
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-14 19:41:30 -0700

    Functional Changes

--- basic_functions.py
+++ basic_functions.py
@@ -1,5 +1,97 @@
-def add(arr, mode = "int"):
-    sum = 0
+def add(arr, mode = "mixed"):
+    """
+    Equivalent of Python's inbuilt sum() function, with compatibility for multiple data types
+    This function takes in an array and a given mode, which can take on one of these data types:
+    * int -- typical integer
+    * float -- typical decimal value
+    * str -- string
+    * mixed -- consisting of all values above
+    Note that in the mixed mode, the answer will be returned as a float value
+    """
+    if(mode == "int" or mode == "float"):
+        sum = 0
+        for j in arr:
+            sum += j
+        return sum
+    elif(mode == "str"):
+        sum = 0
+        for j in arr:
+            sum += float(j)
+        return sum
+    elif(mode == "mixed"):
+        sum = 0
+        for j in arr:
+            summand = float(str(j))
+            sum += summand
+        return sum
+    else:
+        print('Function has failed. Incorrect data type.')
+
+def average(arr, mode = "mixed"):
+    """
+    average(arr, mode) takes the average of a given array
+    Once again, the modes of add() can be used here to denote what the type of the array is
+    The function below, determine_mode(arr) can be used to determine the correct mode for your array
+    """
+    sum = add(arr, mode)
+    answer = sum/len(arr)
+    return answer
+
+def types(arr):
+    """
+    types(arr) will return an array of length equivalent to that of the input array
+    This is analogous to the type() function typically carried out on a singular variable
+    """
+    types = [type(a) for a in arr]
+    return types
+
+def highest_frequency(arr):
+    """
+    highest_frequency(arr) returns an array of length 2
+    This function looks for the number of occurrences of each value in the given array and returns the top results
+    Returned array has the structure: [value with highest frequency, number of occurrences of value]
+    """
+    dictionary = {}
     for j in arr:
-        sum += j
-    return sum
+        if(j in dictionary):
+            dictionary[j] += 1
+        else:
+            dictionary[j] = 1
+    max_value = -10
+    max_key = ""
+    for a in dictionary:
+        if(dictionary[a] > max_value):
+            max_key = a
+            max_value = dictionary[a]
+    return [max_key, max_value]
+
+def frequency(arr):
+    """
+    frequency() returns a dictionary with keys set as elements from the array and values as their respective frequencies
+    Frequency is determined as the number of times occurred in given array
+    """
+    dictionary = {}
+    for j in arr:
+        if(j in dictionary):
+            dictionary[j] += 1
+        else:
+            dictionary[j] = 1
+    return dictionary
+
+def determine_mode(arr):
+    """
+    determine_mode helps determine which mode is necessary for function like add() and average()
+    Note that mixed does not check explicitly for mixed cases and may return an error at runtime if array
+    elements are of unparseable types
+    """
+    type_arr = types(arr)
+    dictionary = frequency(type_arr)
+    if(len(dictionary) == 1):
+        if("int" in str(dictionary.keys())):
+            return "int"
+        elif("float" in str(dictionary.keys())):
+            return "float"
+        else:
+            return "str"
+    else:
+        return "mixed"
--- tests.py
+++ tests.py
@@ -1,18 +1,38 @@
 import basic_functions as bf
 
-def check(real, predicted, test_number):
+def check(real = 0, predicted = 0, test_number = 0, function_name = "Null Test"):
     if(str(real) == str(predicted)):
-        print('Test #' + str(test_number) + ' has passed')
+        print('Test #' + str(test_number) + ' has passed -- ' + function_name)
     else:
-        print('Test #' + str(test_number) + ' has passed')
-        print('Test has failed')
+        print('Test #' + str(test_number) + ' has failed')
         print('Recalibrating, recalibrating....')
 
 def test_add():
     sample_arr = [5,3,1,4,2]
-    check(bf.add(sample_arr), 15, 1)
+    check(bf.add(sample_arr, mode = "int"), 15, 1, "ADD")
+
+def test_add_advanced():
+    sample_arr = ["5",3.0,1,"4",2.0]
+    check(bf.add(sample_arr, mode = "mixed"), 15.0, 3, "ADD - ADVANCED")
+
+def test_highest_frequency():
+    sample_arr = [5,4,19,3,19,5,2,6,7,5]
+    check(bf.highest_frequency(sample_arr), [5,3], 2, "HIGHEST_FREQUENCY")
+
+def test_determine_mode():
+    sample_arr = ["5",3.0,1,"4",2.0]
+    check(bf.determine_mode(sample_arr), "mixed", 4, "DETERMINE_MODE")
+
+def test_determine_mode_advanced():
+    sample_arr = [5,3,1,4,2]
+    check(bf.determine_mode(sample_arr), "int", 4, "DETERMINE_MODE")
+
 
 def run_all():
     test_add()
+    test_highest_frequency()
+    test_add_advanced()
+    test_determine_mode()
+    test_determine_mode_advanced()
 
 run_all()
