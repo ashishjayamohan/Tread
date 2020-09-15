a136ed505e012f05ceaa09f3281428d38908abb8
Matt Fellenz <mattf53190@gmail.com>
2020-09-15 12:25:30 -0700

    Allow custom values for colors, titles, and axis labels

--- graphing_functions.py
+++ graphing_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import basic_functions as bf
 
-def graph_frequency_histogram(arr):
+def graph_frequency_histogram(arr, bar_color='green', title='Graph of Frequencies'):
     """
     This function generates a graph of the frequencies of the elements in the input array, arr
     This function utilizes matplotlib and will produce errors if the package is not installed
@@ -16,12 +16,12 @@ def graph_frequency_histogram(arr):
     values = [dictionary[i] for i in keys]
     x_pos = [i for i in range(len(keys))]
 
-    plt.bar(x_pos, values, color='green')
-    plt.title("Graph of Frequencies")
+    plt.bar(x_pos, values, color=bar_color)
+    plt.title(title)
     plt.xticks(x_pos, keys)
     plt.show()
 
-def graph_scatter(arr):
+def graph_scatter(arr, color='green', title='Scatter Plot of Given Points', x_label='X', y_label='Y'):
     """
     This function returns a scatter plot of the given data
     Note that input array must be an array of array with each sub-array
@@ -34,8 +34,8 @@ def graph_scatter(arr):
 
     fig = plt.figure()
     axes = fig.add_axes([0,0,1,1])
-    axes.scatter(x, y, color = 'green')
-    axes.set_xlabel('X')
-    axes.set_ylabel('Y')
-    axes.set_title('Scatter Plot of Given Points')
+    axes.scatter(x, y, color=color)
+    axes.set_xlabel(x_label)
+    axes.set_ylabel(y_label)
+    axes.set_title(title)
     plt.show()
