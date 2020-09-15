5e0ea4dcf13551f927a37ddc4d89d692da7ada4f
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-14 20:58:01 -0700

    Graphing Functionality

---
+++ graphing_functions.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import basic_functions as bf
+
+def graph_frequency_histogram(arr):
+    """
+    This function generates a graph of the frequencies of the elements in the input array, arr
+    This function utilizes matplotlib and will produce errors if the package is not installed
+    To install matplotlib, use 'pip install matplotlib'
+    """
+    plt.style.use('ggplot')
+
+    dictionary = bf.frequency(arr)
+    keys = dictionary.keys()
+    values = [dictionary[i] for i in keys]
+    x_pos = [i for i in range(len(keys))]
+
+    plt.bar(x_pos, values, color='green')
+    plt.title("Graph of Frequencies")
+    plt.xticks(x_pos, keys)
+    plt.show()
+
+def graph_scatter(arr):
+    """
+    This dunction returns a scatter plot of the given data
+    Note that input array must be an array of array with each sub-array
+    having two elements: [x,y] for the respective point
+    """
+    plt.style.use('ggplot')
+
+    x = [i[0] for i in arr]
+    y = [i[1] for i in arr]
+
+    fig = plt.figure()
+    axes = fig.add_axes([0,0,1,1])
+    axes.scatter(x, y, color = 'green')
+    axes.set_xlabel('X')
+    axes.set_ylabel('Y')
+    axes.set_title('Scatter Plot of Given Points')
+    plt.show()
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import basic_functions as bf
+import graphing_functions as gf
 
 def check(real = 0, predicted = 0, test_number = 0, function_name = "Null Test"):
     if(str(real) == str(predicted)):
@@ -25,14 +26,28 @@ def test_determine_mode():
 
 def test_determine_mode_advanced():
     sample_arr = [5,3,1,4,2]
-    check(bf.determine_mode(sample_arr), "int", 4, "DETERMINE_MODE")
+    check(bf.determine_mode(sample_arr), "int", 5, "DETERMINE_MODE - ADVANCED")
 
+def test_graph_frequency_histogram():
+    sample_arr = [5,4,19,3,19,5,2,6,7,5]
+    gf.graph_frequency_histogram(sample_arr)
+
+def test_graph_scatter():
+    sample_arr = [[5,3], [3,2], [2,1], [4,3], [5,1], [0,2], [1,3]]
+    gf.graph_scatter(sample_arr)
 
 def run_all():
+
     test_add()
     test_highest_frequency()
     test_add_advanced()
     test_determine_mode()
     test_determine_mode_advanced()
+
+    print("----------------------------------")
+    print("BEGINNING GRAPH CHECK")
+    test_graph_frequency_histogram()
+    test_graph_scatter()
+    print("GRAPH CHECK COMPLETE")
 
 run_all()
