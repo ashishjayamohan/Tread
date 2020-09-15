91624c9f00731661bc948a0c5573e3459e466afc
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-15 16:47:04 -0700

    Grammatical Changes Determinant Function
    - Got rid of comments
    - Fixed up TypeError Statement

--- matrix_functions.py
+++ matrix_functions.py
@@ -1,18 +1,18 @@
 def determinant(matrix):
     if (len(matrix) != len(matrix[0])):
-        raise TypeError('Matrix must be square.')
+        raise TypeError('Matrix must be a square')
     else:
         size = len(matrix)
         if (size == 1):
-            return matrix[0][0] # the determinant of a 1x1 matrix is the value of the only element in the matrix.
+            return matrix[0][0]
         else:
-            newmatrix = matrix[1:] # take off top row of matrix
-            ret = 0 # keep track of return value
-            for i in range(size): # for each top row element
-                element = matrix[0][i] # get that element
+            newmatrix = matrix[1:]
+            ret = 0
+            for i in range(size):
+                element = matrix[0][i]
                 minor = list(row[0:i]+row[i+1:size] for row in newmatrix)
-                minor_determinant = determinant(minor) # find the determinant of that element's minor
-                if (i % 2 == 0): # alternate between adding and subtracting their product
+                minor_determinant = determinant(minor)
+                if (i % 2 == 0):
                     ret += element * minor_determinant
                 else:
                     ret -= element * minor_determinant
