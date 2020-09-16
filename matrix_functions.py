3c290f7e3c698cd7381d2b85cbebecd05b36d821
agorpynich <oleksandr.gorpynich@gmail.com>
2020-09-15 19:04:54 -0700

    added the element_wise_multiply for 2 matrices

--- matrix_functions.py
+++ matrix_functions.py
@@ -1,6 +1,6 @@
 def determinant(matrix):
     if len(matrix) != len(matrix[0]):
-        raise TypeError('Table must be a square')
+        raise TypeError("Table must be a square")
     else:
         size = len(matrix)
         if size == 1:
@@ -17,6 +17,7 @@ def determinant(matrix):
                 else:
                     ret -= element * minor_determinant
             return ret
+
 
 def multiply(matrix1, matrix2):
     new_matrix1 = []
@@ -49,6 +50,38 @@ def multiply(matrix1, matrix2):
 
     return result_matrix
 
+
+def element_wise_multiply(matrix1, matrix2):
+    new_matrix1 = []
+    new_matrix2 = []
+    if not (isinstance(matrix1[0], list)):
+        new_matrix1 = [[float(item) for item in matrix1]]
+    else:
+        for x in range(len(matrix1)):
+            new_matrix1.append([])
+            for y in range(len(matrix1[x])):
+                new_matrix1[x].append(float(matrix1[x][y]))
+
+    if not (isinstance(matrix2[0], list)):
+        new_matrix2 = [[float(item) for item in matrix2]]
+    else:
+        for x in range(len(matrix2)):
+            new_matrix2.append([])
+            for y in range(len(matrix2[x])):
+                new_matrix2[x].append(float(matrix2[x][y]))
+    if len(new_matrix1) != len(new_matrix2) or len(new_matrix1[0]) != len(new_matrix2[0]):
+        raise TypeError(
+            f"Cannot point-wise multiply matrices of the size {len(new_matrix1)} x {len(new_matrix1[0])} and {len(new_matrix2)} x {len(new_matrix2[0])}"
+        )
+    else:
+        result_matrix = []
+        for x in range(len(new_matrix1)):
+            result_matrix.append([])
+            for y in range(len(new_matrix1[0])):
+                result_matrix[x].append(new_matrix1[x][y] * new_matrix2[x][y])
+    return result_matrix
+
+
 def add_column_values(table, column_title, column_values):
     """
     Adds a column to a specific table
@@ -59,6 +92,7 @@ def add_column_values(table, column_title, column_values):
         table[j].append(column_values[j])
     return table
 
+
 def remove_column(table, index):
     """
     Removes a column from a given table given the column index
@@ -67,11 +101,13 @@ def remove_column(table, index):
         j.pop(index)
     return table
 
+
 def extract_column(table, index):
     """
     Returns the values of a specific column in a table given the column index
     """
     return [j[index] for j in table]
+
 
 def vector_function_column(table, function, index):
     """
