1180dfdaf12ff79f90e8ed586848e8857dd81e25
OlexG <36348190+OlexG@users.noreply.github.com>
2020-09-15 19:27:44 -0700

    added the element_wise_multiply for 2 matrices (#7)
    * added the element_wise_multiply for 2 matrices (OlexG)
    
    * Undo edit to determinant fn (mattfbacon)
    
    * Added function that formats a matrix (OlexG)
    
    Co-authored-by: Matt Fellenz <mattf53190@gmail.com>

--- matrix_functions.py
+++ matrix_functions.py
@@ -18,24 +18,21 @@ def determinant(matrix):
                     ret -= element * minor_determinant
             return ret
 
-def multiply(matrix1, matrix2):
-    new_matrix1 = []
-    new_matrix2 = []
-    if not (isinstance(matrix1[0], list)):
-        new_matrix1 = [[float(item) for item in matrix1]]
-    else:
-        for x in range(len(matrix1)):
-            new_matrix1.append([])
-            for y in range(len(matrix1[x])):
-                new_matrix1[x].append(float(matrix1[x][y]))
 
-    if not (isinstance(matrix2[0], list)):
-        new_matrix2 = [[float(item) for item in matrix2]]
+def make_matrix(old_matrix):
+    new_matrix = [];
+    if not (isinstance(old_matrix[0], list)):
+        new_matrix = [[float(item) for item in old_matrix]]
     else:
-        for x in range(len(matrix2)):
-            new_matrix2.append([])
-            for y in range(len(matrix2[x])):
-                new_matrix2[x].append(float(matrix2[x][y]))
+        for x in range(len(old_matrix)):
+            new_matrix.append([])
+            for y in range(len(old_matrix[x])):
+                new_matrix[x].append(float(old_matrix[x][y]))
+    return new_matrix
+
+def multiply(matrix1, matrix2):
+    new_matrix1 = make_matrix(matrix1)
+    new_matrix2 = make_matrix(matrix2)
     if len(new_matrix1[0]) != len(new_matrix2):
         raise TypeError(
             f"Cannot multiply matrices of the size {len(new_matrix1)} x {len(new_matrix1[0])} and {len(new_matrix2)} x {len(new_matrix2[0])}"
@@ -49,6 +46,23 @@ def multiply(matrix1, matrix2):
 
     return result_matrix
 
+
+def element_wise_multiply(matrix1, matrix2):
+    new_matrix1 = make_matrix(matrix1)
+    new_matrix2 = make_matrix(matrix2)
+    if len(new_matrix1) != len(new_matrix2) or len(new_matrix1[0]) != len(new_matrix2[0]):
+        raise TypeError(
+            f"Cannot point-wise multiply matrices of the size {len(new_matrix1)} x {len(new_matrix1[0])} and {len(new_matrix2)} x {len(new_matrix2[0])}"
+        )
+    else:
+        result_matrix = []
+        for x in range(len(new_matrix1)):
+            result_matrix.append([])
+            for y in range(len(new_matrix1[0])):
+                result_matrix[x].append(new_matrix1[x][y] * new_matrix2[x][y])
+    return result_matrix
+
+
 def add_column_values(table, column_title, column_values):
     """
     Adds a column to a specific table
@@ -59,6 +73,7 @@ def add_column_values(table, column_title, column_values):
         table[j].append(column_values[j])
     return table
 
+
 def remove_column(table, index):
     """
     Removes a column from a given table given the column index
@@ -67,11 +82,13 @@ def remove_column(table, index):
         j.pop(index)
     return table
 
+
 def extract_column(table, index):
     """
     Returns the values of a specific column in a table given the column index
     """
     return [j[index] for j in table]
+
 
 def vector_function_column(table, function, index):
     """
