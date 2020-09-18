6164adeeab7b52a1d8a6d832013a1b15bbbdd809
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-17 22:19:57 -0700

    Update Test Program
    Added comments for readability

--- test_program.py
+++ test_program.py
@@ -1,5 +1,10 @@
 from scripts import basic_functions as bf
 
+"""
+Import necessary files from 'scripts' folder and use 'as' to specify the alias of the module
+After that, you can use the alias to use functions from the script
+"""
+
 sample_arr = [5,4,3,2]
 sum_sample_arr = bf.add(sample_arr)
 print(sum_sample_arr)
