da35def7b29a335b4dfd4ae033dc0be9ee0b4107
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-19 21:09:21 -0700

    Math Function Files Addition

---
+++ scripts/math_functions.py
@@ -0,0 +1,25 @@
+def add_num(a, b):
+    """
+    Given two numbers, this function will return the sum of these
+    Will be returned in whichever type input is given (int, float)
+    """
+    return a + b
+
+def subtract_num(a, b):
+    """
+    Given two numbers, this function will deduct the second number from the first
+    Returned numerical's type is of input's type
+    """
+    return a - b
+
+def abs_diff(a, b):
+    """
+    Given two numbers, this function will return the absolute difference between the two
+    """
+    return abs(a - b)
+
+def easter():
+    # Le Undocumented Function
+    print("Wow, you found this.")
+    print("Good job, I just wasted 2 seconds of your time.")
+    print("-----------------------------------------------")
