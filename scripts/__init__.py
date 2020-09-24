615c285954ff48412e1c1ac957d04961d0e5dc77
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-24 09:34:47 -0700

    Update __init__.py

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+print("----------------------------------------------")
+print("Tread Python Package - Data Science ----------")
+print("Made By: Ashish Jayamohan --------------------")
+print("Get Started By Running tests.py --------------")
+print("----------------------------------------------")
